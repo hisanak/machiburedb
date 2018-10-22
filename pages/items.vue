1a8cef6525631b711fa847f8812c1acb3c2b00f9
hisanak <terada@asg.cs.tuat.ac.jp>
2018-10-23 03:51:53 +0900

    add color to quest levels

--- pages/items.vue
+++ pages/items.vue
@@ -1,25 +1,32 @@
 <template>
   <div class="mbc-items">
     <h1>素材名から検索</h1>
+    <div class="sel_list">
+      <div class="sel_item" v-for="group in groups" :key="group.id">
+        <div class="item_bundle">
+          <input type="checkbox" :id="group.id" :value="group.id" v-model="checked_groups" />
+          <label :for="group.id">{{ group.name }}</label>
+        </div>
+      </div>
+    </div>
     <div class="sel_list" v-if="showItems">
       <div class="sel_item" v-for="item in items" :key="item.item_id">
         <div class="item_bundle">
-          <input type="checkbox" :id="item.sel_id" :value="item.sel_id" v-model="checked" />
+          <input type="checkbox" :id="item.sel_id" :value="item.sel_id" v-model="checked_items" />
           <label :for="item.sel_id">{{ item.item_name }}</label>
         </div>
       </div>
     </div>
-    <!--<p>{{ checked }}</p>-->
     <div class="quest_list">
-      <div v-if="showNotCompleted">
-        <strong>※未完成(更新中)</strong>
-      </div>
       <div class="seled_quest" v-if="showQuests">
-        <div v-if="quests.length == 0">
-          <span>No Result</span>
+        <div v-if="showNotCompleted">
+          <p><strong>※未完成(更新中)</strong></p>
         </div>
-        <div v-else>
-          <p v-for="quest in quests" :key="quest.id">{{ parseQuests(quest) }}</p>
+        <div v-if="quest_ids.length == 0">
+          <p>No Result</p>
+        </div>
+        <div v-else v-for="quest in quests" :key="quest.id">
+            <p>{{ quest.name }}（<span v-if="quest.n" style="color: #606060;">{{ quest.n }}</span>{{ quest.n_h }}<span v-if="quest.h" style="color: #C00000;">{{ quest.h }}</span>{{ quest.h_e }}<span v-if="quest.e" style="color: #800080;">{{ quest.e }}</span>）</p>
         </div>
       </div>
     </div>
@@ -36,9 +43,12 @@ export default {
       quest_world1_data: [],
       quest_week_data: [],
       quest_event_data: [],
+      groups: [{name: 'ブキ素材', id: 'W'}, {name: 'スピリット素材', id: 'S'}, {name: '玉素材', id: 'B'}, {name: 'イベント素材', id: 'E'}],
+      checked_groups: [],
       items: [],
-      checked: [],
-      quests: [{}],
+      checked_items: [],
+      quests: [],
+      quest_ids: [],
       showItems: false,
       showQuests: false,
       showNotCompleted: false,
@@ -57,11 +67,11 @@ export default {
     }
   },
   watch: {
-    checked: {
+    checked_items: {
       handler: function(newValue) {
         this.showQuests = false;
-        this.showNotCompleted = false;
         this.narrowQuests();
+        this.parseQuests();
         this.showQuests = true;
       }
     }
@@ -87,27 +97,74 @@ export default {
       }
     },
     narrowQuests: function() {
-      if (this.checked.length == 0) {
-        this.quests = [{}];
+      if (this.checked_items.length == 0) {
+        this.quest_ids = [];
         return;
       }
-      this.quests = this.getQuests(0);
-      for (let i = 1; i < this.checked.length; i++) {
+      this.quest_ids = this.getQuests(0);
+      for (let i = 1; i < this.checked_items.length; i++) {
         const b = new Set(this.getQuests(i));
-        const a = new Set(this.quests);
+        const a = new Set(this.quest_ids);
         const a_and_b = new Set([...a].filter(e => (b.has(e))));
-        this.quests = [...a_and_b];
+        this.quest_ids = [...a_and_b];
       }
 //      console.log(this.quests);
     },
     getQuests: function(index) {
       // 'selXYZ' -> int('XYZ')
-      const id = parseInt(this.checked[index].slice(3), 10);
+      const id = parseInt(this.checked_items[index].slice(3), 10);
       const raw_quests = this.item_data[id].quest;
       const quests = raw_quests.map(x => x.split(':')[0]);
       return quests;
     },
-    parseQuests: function(quest_id) {
+    parseQuests: function() {
+      this.showNotCompleted = false;
+      this.quests = [];
+      var quests = [];
+      for (let i = 0; i < this.quest_ids.length; i++) {
+        const quest_id = this.quest_ids[i];
+        const qid = quest_id.split('-');
+        try {
+          if (qid[0] == '1') {
+            const l1 = parseInt(qid[1], 10) - 1;
+            const l2 = parseInt(qid[2], 10) - 1;
+            let quest = {name: this.quest_world1_data[l1].name + ': ' + this.quest_world1_data[l1].quest[l2].name};
+            this.parseLevels(quest, qid[3]);
+            quests.push(quest);
+          }
+          else if (qid[0] == 'E') {
+            const l1 = parseInt(qid[1], 10) - 1;
+            let quest = {name: this.quest_event_data[l1].name};
+            this.parseLevels(quest, qid[2]);
+            quests.push(quest);
+          }
+          else if (qid[0] == 'W') {
+            const l2 = parseInt(qid[2], 10) - 1;
+            let quest = {name: this.quest_week_data[qid[1]].name + ': ' + this.quest_week_data[qid[1]].quest[l2].name};
+            this.parseLevels(quest, qid[3]);
+            quests.push(quest);
+          }
+          else if (qid[0] == 'U') {
+            const l1 = parseInt(qid[1], 10) - 1;
+            let quest = {name: "不明: " + this.quest_unknown_data[l1].name};
+            this.parseLevels(quest, qid[2]);
+            quests.push(quest);
+          }
+          else if (qid[0] == 'M') {
+            this.showNotCompleted = true;
+          }
+          else {
+            throw 'Unknown Class';
+          }
+        }
+        catch (e) {
+          console.log("parsing:", qid, "(orig:", quest_id);
+          console.log(e);
+        }
+      }
+      this.quests = quests;
+    },
+    parseQuest: function(quest_id) {
       if (Object.keys(quest_id).length == 0) {
         return 'No Result';
       }
@@ -116,7 +173,9 @@ export default {
         if (qid[0] == '1') {
           const l1 = parseInt(qid[1], 10) - 1;
           const l2 = parseInt(qid[2], 10) - 1;
-          return this.quest_world1_data[l1].name + ': ' + this.quest_world1_data[l1].quest[l2].name + this.parseLevels(qid[3]);
+          let quest = {name: this.quest_world1_data[l1].name + ': ' + this.quest_world1_data[l1].quest[l2].name}
+          this.parseLevels(quest, qid[3]);
+//            + this.parseLevels(qid[3]);
         }
         else if (qid[0] == 'E') {
           const l1 = parseInt(qid[1], 10) - 1;
@@ -144,9 +203,36 @@ export default {
       }
       return 'Not Found';
     },
+    /*
     parseLevels(levels) {
       let larr = [...levels].map(x => this.parseLevel(x));
       return '(' + larr + ')';
+    },
+    */
+    parseLevels(quest, desc) {
+      const l2j = {N: 'ノーマル', H: 'ハード', E: 'エクストラ'};
+      for (let i = 0; i < desc.length; i++) {
+        if (desc[i] == 'N') {
+          quest.n = 'ノーマル';
+        }
+        else if (desc[i] == 'H') {
+          quest.h = 'ハード';
+        }
+        else if (desc[i] == 'E') {
+          quest.e = 'エクストラ';
+        }
+
+        quest.n_h = quest.h_e = null;
+        if (quest.n && quest.h) {
+          quest.n_h = '、';
+        }
+        if (quest.h && quest.e) {
+          quest.h_e = '、';
+        }
+        if (quest.n && quest.h && quest.n_h == null && quest.h_e == null) {
+          quest.n_h = '、';
+        }
+      }
     },
     parseLevel(level) {
       const l2j = {N: 'ノーマル', H: 'ハード', E: 'エクストラ'};
